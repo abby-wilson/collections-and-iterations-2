964feaad3d1f1979e2ed9b6443d72336a0c3116a
Abby Wilson <abigail.wilson1@outlook.com>
2017-06-28 15:09:40 -0400

    exercise 1

---
+++ exercises.rb
@@ -0,0 +1,29 @@
+fav_colours = ["red", "blue", "green", "orange"]
+ages = [20, 16, 30, 45]
+heads_five_flip = ["yes", "no", "yes", "no", "no"]
+performing_artists = ["james", "amy", "maverick"]
+fav_colours_symbols = [:red, :blue, :green, :orange]
+
+words = {
+  :hit => "bring one's hand or a tool or weapon into contact with (someone or something) quickly and forcefully.",
+  :cat => "a small domesticated carnivorous mammal with soft fur, a short snout, and retractile claws.",
+  :car => "a road vehicle, typically with four wheels, powered by an internal combustion engine and able to carry a small number of people."
+}
+
+movies = {
+  :Shawshank => 2010,
+  :Avatar => 2000,
+  :Inception => 2010
+}
+
+cities = {
+  :Toronto => 6418000,
+  :Glasgow => 598830,
+  :Edinburgh => 495360
+}
+
+names = {
+  :Freddie => 16,
+  :Katie => 21,
+  :Sarah => 22
+}
